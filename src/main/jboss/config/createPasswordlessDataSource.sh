aeaf4649535cba9725f3d8cc624c5fabb170ab1a
lcephas <73600344+lcephas@users.noreply.github.com>
2024-10-24 10:27:44 +0000

    fix bad substitution

--- src/main/jboss/config/createPasswordlessDataSource.sh
+++ src/main/jboss/config/createPasswordlessDataSource.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-$JBOSS_HOME/bin/jboss-cli.sh --connect <<EOF
-data-source add --name=CredentialFreeDataSourceDS \
---jndi-name=java:jboss/datasources/CredentialFreeDataSource \
+$JBOSS_HOME/bin/jboss-cli.sh --connect --resolve-parameter-values <<EOF
+data-source add --name=AZURE_MYSQL_CONNECTIONSTRING_MANAGED_ID_DS \
+--jndi-name=java:jboss/env/jdbc/AZURE_MYSQL_CONNECTIONSTRING_MANAGED_ID_DS \
 --connection-url=${env.AZURE_MYSQL_CONNECTIONSTRING}&defaultAuthenticationPlugin=com.azure.identity.extensions.jdbc.mysql.AzureMysqlAuthenticationPlugin&authenticationPlugins=com.azure.identity.extensions.jdbc.mysql.AzureMysqlAuthenticationPlugin \
 --driver-name=mysql \
 --use-ccm=true \
